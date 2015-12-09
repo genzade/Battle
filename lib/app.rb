37697c9624700145550350132202e415c1a214e9
Sara Tateno <sara.tateno@gmail.com>
2015-12-09 15:46:54 +0000

    First user story refactor: Post Redirect Get pattern

--- lib/app.rb
+++ lib/app.rb
@@ -1,14 +1,25 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
     p params
-    @username_1 = params[:username_1] 
-    @username_2 = params[:username_2]
+    p session
+    session[:username_1] = params[:username_1]
+    session[:username_2] = params[:username_2]
+    p session
+    redirect '/play'
+  end
+
+  get '/play' do
+    @username_1 = session[:username_1]
+    @username_2 = session[:username_2]
     erb(:play)
   end
 
